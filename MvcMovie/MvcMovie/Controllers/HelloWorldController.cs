21098779381783d434598d4cc3d90fad4ae1e956
sudo.n <takokiti1001@gmail.com>
2022-05-26 23:21:38 +0900

    firast

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name , int numTimes　= 1)
+        {
+            ViewData["Message"] = $"こんにちは！{name}さん";
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
