b8616fa85f9a9c486f36d55e1ab19cb27680465c
sudo.n <takokiti1001@gmail.com>
2022-05-28 19:28:26 +0900

    d
    Signed-off-by:  <takokiti1001@gmail.com>

---
+++ MvcMovie/MvcMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContex : DbContext 
+    {
+        public MvcMovieContex(DbContextOptions<MvcMovieContex> options)
+            : base(options)
+        { 
+            
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Cenre { get; set; }
+        public decimal Revenue { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+    }
+}
